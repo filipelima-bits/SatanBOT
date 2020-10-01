85682ab928031bed80b6b36e7e7f04288d1c9eb6
filipelima-bits <ffilipe.dev@gmail.com>
2020-09-30 22:07:57 -0300

    Two events

--- events/guildBanAdd.js
+++ events/guildBanAdd.js
@@ -7,50 +7,48 @@ module.exports = (client, guild, user) => {
 		// GuildAuditLogsEntry for this user and type
 		if (user.id == logs.entries.first().target.id) {
 			banMember = logs.entries.first().target
-            client.conlog(`${banMember.tag} (id: ${banMember.id}) has been banned from the server ${guild.name}`)
-            // Erro na linha acima: client.conlog() is not a function
-			let modlog = guild.channels.get(process.env.TESTCHANNEL);
-			if (!modlog) return console.log(chalk.redBright("Log Channel doesn't exist!!"));
+			let modlog = client.channels.cache.get(process.env.MODLOGS);
+			if (!modlog) return console.log(chalk.redBright("Canal de logs de moderação não existe!!"));
             
             var embedBanOne = new Discord.MessageEmbed()
-				.setColor(0x212121)
+				.setColor('#fa1b1b')
 				.setTimestamp()
                 .addField('Action', "Ban", true)
                 .setTitle('Aurevoir')
 				.addField('Target', `${banMember.tag} `, true)
 				.addField('Moderator', logs.entries.first().executor.tag, true)
 				.addField('Reason', 'Please provide a reason for ban:', false)
-                .setFooter(`User banned (${banMember.id})`, banMember.displayAvatarURL());
+                .setFooter(`Usuário banido (${banMember.id})`, banMember.displayAvatarURL());
                 
             var embedBanTwo = new Discord.MessageEmbed()
-				.setColor(0x212121)
+				.setColor('#fa1b1b')
 				.setTimestamp()
                 .addField('Action', "Ban", true)
                 .setTitle('Alguém aprontou em...')
 				.addField('Target', `${banMember.tag} `, true)
 				.addField('Moderator', logs.entries.first().executor.tag, true)
 				.addField('Reason', 'Please provide a reason for ban:', false)
-                .setFooter(`User banned (${banMember.id})`, banMember.displayAvatarURL());
+                .setFooter(`Usuário banido (${banMember.id})`, banMember.displayAvatarURL());
             
             var embedBanThree = new Discord.MessageEmbed()
-				.setColor(0x212121)
+				.setColor('#fa1b1b')
 				.setTimestamp()
                 .addField('Action', "Ban", true)
                 .setTitle('Culpado!')
 				.addField('Target', `${banMember.tag} `, true)
 				.addField('Moderator', logs.entries.first().executor.tag, true)
 				.addField('Reason', 'Please provide a reason for ban:', false)
-				.setFooter(`User banned (${banMember.id})`, banMember.displayAvatarURL());
+				.setFooter(`Usuário banido (${banMember.id})`, banMember.displayAvatarURL());
             
             var embedBanFour = new Discord.MessageEmbed()
-				.setColor(0x212121)
+				.setColor('#fa1b1b')
 				.setTimestamp()
                 .addField('Action', "Ban", true)
                 .setTitle('Pensou que sairia impune? jamais!')
 				.addField('Target', `${banMember.tag} `, true)
 				.addField('Moderator', logs.entries.first().executor.tag, true)
 				.addField('Reason', 'Please provide a reason for ban:', false)
-				.setFooter(`User banned (${banMember.id})`, banMember.displayAvatarURL());
+				.setFooter(`Usuário banido (${banMember.id})`, banMember.displayAvatarURL());
             
             let choice = Math.floor(Math.random() * 4);
 
