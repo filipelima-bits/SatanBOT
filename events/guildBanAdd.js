52f056a71f1eed539d35960a80589e94789c624b
filipelima-bits <ffilipe.dev@gmail.com>
2020-10-04 03:32:07 -0300

    updates

--- events/guildBanAdd.js
+++ events/guildBanAdd.js
@@ -7,60 +7,19 @@ module.exports = (client, guild, user) => {
 		// GuildAuditLogsEntry for this user and type
 		if (user.id == logs.entries.first().target.id) {
 			banMember = logs.entries.first().target
-			let modlog = client.channels.cache.get(process.env.MODLOGS);
+			let modlog = client.channels.cache.get(process.env.TESTCHANNEL);
 			if (!modlog) return console.log(chalk.redBright("Canal de logs de moderação não existe!!"));
             
-            var embedBanOne = new Discord.MessageEmbed()
+            var embedBan = new Discord.MessageEmbed()
+				// .addField('Action', "Ban", true)
 				.setColor('#fa1b1b')
 				.setTimestamp()
-                .addField('Action', "Ban", true)
-                .setTitle('Aurevoir')
-				.addField('Target', `${banMember.tag} `, true)
-				.addField('Moderator', logs.entries.first().executor.tag, true)
-				.addField('Reason', 'Please provide a reason for ban:', false)
-                .setFooter(`Usuário banido (${banMember.id})`, banMember.displayAvatarURL());
-                
-            var embedBanTwo = new Discord.MessageEmbed()
-				.setColor('#fa1b1b')
-				.setTimestamp()
-                .addField('Action', "Ban", true)
-                .setTitle('Alguém aprontou em...')
-				.addField('Target', `${banMember.tag} `, true)
-				.addField('Moderator', logs.entries.first().executor.tag, true)
-				.addField('Reason', 'Please provide a reason for ban:', false)
-                .setFooter(`Usuário banido (${banMember.id})`, banMember.displayAvatarURL());
+				.setTitle('Membro banido !!')
+				.setDescription(`O membro ${banMember.tag} foi banido pelo staffer ${logs.entries.first().executor.tag}`)
+                .setFooter(`ID: (${banMember.id})`, banMember.displayAvatarURL());
             
-            var embedBanThree = new Discord.MessageEmbed()
-				.setColor('#fa1b1b')
-				.setTimestamp()
-                .addField('Action', "Ban", true)
-                .setTitle('Culpado!')
-				.addField('Target', `${banMember.tag} `, true)
-				.addField('Moderator', logs.entries.first().executor.tag, true)
-				.addField('Reason', 'Please provide a reason for ban:', false)
-				.setFooter(`Usuário banido (${banMember.id})`, banMember.displayAvatarURL());
-            
-            var embedBanFour = new Discord.MessageEmbed()
-				.setColor('#fa1b1b')
-				.setTimestamp()
-                .addField('Action', "Ban", true)
-                .setTitle('Pensou que sairia impune? jamais!')
-				.addField('Target', `${banMember.tag} `, true)
-				.addField('Moderator', logs.entries.first().executor.tag, true)
-				.addField('Reason', 'Please provide a reason for ban:', false)
-				.setFooter(`Usuário banido (${banMember.id})`, banMember.displayAvatarURL());
-            
-            let choice = Math.floor(Math.random() * 4);
-
-            if (choice === 0) {
-                modlog.send({embed: embedBanOne}).catch();
-            } else if (choice === 1) {
-                modlog.send({embed: embedBanTwo}).catch();
-            } else if (choice === 2) {
-                modlog.send({embed: embedBanThree}).catch();
-            } else {
-                modlog.send({embed: embedBanFour}).catch();
-		    } 
+            modlog.send({embed: embedBan}).catch();
+		     
         }
     });
 };
--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -4,33 +4,15 @@ const moment = require('moment');
 const Discord = require('discord.js');
 
 module.exports = async (client, member) => {
+    
+    // Proteção contra selfbots
+
     const daysSinceCreation = moment().diff(moment(member.user.createdAt), 'days');
     const domaincount = member.user.username.match(/\b((?=[a-z0-9-]{1,63}\.)(xn--)?[a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,63}\b/);
     if (domaincount > 0 || daysSinceCreation < 3) return (() => {
-        member.send('Hey, você foi kickado automáticamente do servidor por suspeitas de divulgação. Contas com menos de 3 dias não podem entrar ao servidor também').catch();
+        member.send('Hey, você foi kickado automáticamente do servidor por suspeitas de divulgação / contas com menos de 3 dias não podem entrar ao servidor').catch();
         member.kick('Autokick: Divulgação ou conta muito recente!').catch();
     });
 
-    const welcomeMessage = new Discord.MessageEmbed()
-        .setThumbnail(`${member.displayAvatarURL}`)
-        .setColor('#fa1b1b')
-        .setTitle('🎈 Bem-vindo(a)!')
-        .setDescription(`
-        -ˏˋ Olá ${member}, seja bem-vindo(a)! ˎˊ-  
-
-        ╭─► 🎀 | **Passe nos canais abaixo:**
-        ╰────────────────────
-        
-        ┌───────────────────┐
-        ┇● #°๑ˑ₍📋₎│registro ➩ Registre-se!
-        ┇● #°๑ˑ₍🧸₎│orientações ➩ Veja as orientações!
-        ┇● #°๑ˑ₍🌈₎│cores ➩ Cores para nick!
-        ┇● #°๑ˑ₍🏓₎│pings ➩ Para notificações!
-        └───────────────────┘
-
-        `)
-        .setFooter(`${member} (id: ${member.id})`, member.displayAvatarURL())
-        .setTimestamp()
-
-        client.channels.cache.get(process.env.TESTCHANNEL).send(welcomeMessage).catch();
+    
 }
